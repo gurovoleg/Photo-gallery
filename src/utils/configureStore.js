cf7f002cdd25d235f7627e13e9fe5b3a250b1b0d
Oleg Gurov <gogotheog@gmail.com>
2021-06-26 18:18:58 +0300

    fixed semantic styles

--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -7,6 +7,14 @@ import createSagaMiddleware from 'redux-saga'
 import rootSaga from 'Sagas'
 
 // export const history = createBrowserHistory()
+
+/*
+GitHub Pages doesn’t support routers that use the HTML5 pushState history API under the hood
+(for example, React Router using browserHistory). This is because when there is a fresh page load for a url
+like http://user.github.io/todomvc/todos/42, where /todos/42 is a frontend route, the GitHub Pages server returns 404
+because it knows nothing of /todos/42. If you want to add a router to a project hosted on GitHub Pages:
+You could switch from using HTML5 history API to routing with hashes. If you use React Router, you can switch to hashHistory for this effect.
+ */
 export const history = createHashHistory() // для GitHub Pages используем hash history
 
 const configureStore = () => {
